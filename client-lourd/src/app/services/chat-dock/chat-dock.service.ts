cd0a569c1462212b88a617ebf0ce130b427d982a
hubertgodin <hubert.godin@polymtl.ca>
2025-09-01 12:35:15 -0400

    feat(chat-popup):added storage and chat dock services

---
+++ client-lourd/src/app/services/chat-dock/chat-dock.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class ChatDockService {
+    // totalQuantity: Subject<number> = new BehaviorSubject<number>(0);
+
+    isEmbeded = true;
+
+    constructor() {}
+}
